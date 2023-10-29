e1f2d72bb76320d3a2d714407c94e005e6981e15
Artem Adiev <92643752+Depechist@users.noreply.github.com>
2023-10-29 19:34:11 +0400

    Скриншот-тест
    - Добавлен скриншот-тест, который успешно пройден

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,25 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Artem Adiev on 29.10.2023.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+final class TrackerTests: XCTestCase {
+    
+    final class TrackerTests: XCTestCase {
+        func testTrackersViewControllerLightTheme() {
+            let vc = TrackersViewController()
+            assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)))
+        }
+        
+        func testTrackersViewControllerDarkTheme() {
+            let vc = TrackersViewController()
+            assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)))
+        }
+    }
+}
